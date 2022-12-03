ff436b129422f99fc6925277d7c4bdf478751b88
Nguyen Kim Tung <72078935+tungnk181120@users.noreply.github.com>
2022-12-03 16:10:19 +0700

    init project

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import GlobalStyles from './components/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
