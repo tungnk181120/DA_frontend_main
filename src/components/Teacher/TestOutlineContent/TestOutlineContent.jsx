11114301aba625725e5b216730758f94d271070e
Nguyen Kim Tung <72078935+tungnk181120@users.noreply.github.com>
2022-12-07 23:33:41 +0700

    test outline

---
+++ src/components/Teacher/TestOutlineContent/TestOutlineContent.jsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(name, subject, time, action) {
+  return { name, subject, time, action };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24),
+  createData('Ice cream sandwich', 237, 9.0, 37),
+  createData('Eclair', 262, 16.0, 24),
+  createData('Cupcake', 305, 3.7, 67),
+  createData('Gingerbread', 356, 16.0, 49),
+];
+
+function TestOutlineContent() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Tên khung đề</TableCell>
+            <TableCell align="right">Môn học</TableCell>
+            <TableCell align="right">Thời gian</TableCell>
+            <TableCell align="right">Hành động</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.subject}</TableCell>
+              <TableCell align="right">{row.time}</TableCell>
+              <TableCell align="right">{row.action}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+
+export default TestOutlineContent;
--- src/pages/Teacher/TestOutline/index.jsx
+++ src/pages/Teacher/TestOutline/index.jsx
@@ -1,9 +1,21 @@
 import Sider from "../../../components/Sider/Sider";
-
+import TestOutlineContent from "../../../components/Teacher/TestOutlineContent/TestOutlineContent";
+import Button from '@mui/material/Button';
+import './TestOutline.scss'
 function TestOutline() {
     return (
-        <div>
+        <div className="test-outline">
             <Sider></Sider>
+            <div className="test-outline-content">
+                <div>
+                    <span className="title-test-outline">Danh sách khung đề thi</span>
+                    <Button variant="contained" color="success" className="btn-create-test-outline" >
+                        Tạo khung đề thi
+                    </Button>
+                </div>                
+                <TestOutlineContent></TestOutlineContent>
+            </div>
+            
         </div>        
     )
 }
